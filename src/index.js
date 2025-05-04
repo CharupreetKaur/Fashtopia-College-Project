77daeb8379231d5e5912243ae8fcc7e80d38635f
Charupreet Kaur <kaurcharupreet@gmail.com>
2025-05-04 13:19:09 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+
+// import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/auth";
+// import { SearchProvider } from "./context/search";
+import { CartProvider } from "./context/cart";
+import "antd/dist/reset.css";
+
+ReactDOM.render(
+  <>
+
+ <AuthProvider>
+ <CartProvider>
+   <BrowserRouter>
+   
+    <App/>
+    
+    </BrowserRouter>
+    </CartProvider>
+    </AuthProvider>
+  </>,
+  document.getElementById('root')
+ 
+  //   <SearchProvider>
+  //     <CartProvider>
+  //        <BrowserRouter>
+  //       
+  //       </BrowserRouter>
+  //     </CartProvider>
+
+  //   </SearchProvider>
+  // </AuthProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
